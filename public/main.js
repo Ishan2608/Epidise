d35f836b229675f9f484449349b33e42a18c696c
IshanRastogi2608 <ishan.developerguide@gmail.com>
2025-02-02 19:37:52 +0530

    Responsive App Feature Set. Mockups Remaining

--- public/main.js
+++ public/main.js
@@ -18,4 +18,25 @@ document.addEventListener("DOMContentLoaded", function () {
     mobileNav.classList.remove("active");
     bodyOverlay.classList.remove("active");
   });
+
+  const t1Button = document.getElementById("t1");
+  const t2Button = document.getElementById("t2");
+  const featuresT1 = document.querySelector("#app-features-t1");
+  const featuresT2 = document.querySelector("#app-features-t2");
+
+  t1Button.addEventListener("click", function () {
+    featuresT1.classList.remove("hidden");
+    featuresT2.classList.add("hidden");
+    t1Button.classList.add("active-toggle");
+    t2Button.classList.remove("active-toggle");
+  });
+
+  t2Button.addEventListener("click", function () {
+    featuresT1.classList.add("hidden");
+    featuresT2.classList.remove("hidden");
+    t1Button.classList.remove("active-toggle");
+    t2Button.classList.add("active-toggle");
+  });
+
 });
+
